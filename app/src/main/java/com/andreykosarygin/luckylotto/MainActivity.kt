57efaf4f2db927585a541ba84200f16249d90a30
Andrey Kosarygin <gaio-gulio-cesare@yandex.ru>
2023-07-09 08:53:06 +0300

    Сделал ScreenGame с анимацией автомата

--- app/src/main/java/com/andreykosarygin/luckylotto/MainActivity.kt
+++ app/src/main/java/com/andreykosarygin/luckylotto/MainActivity.kt
@@ -3,15 +3,33 @@ package com.andreykosarygin.luckylotto
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import com.andreykosarygin.main_ui.screen_main.ScreenMain
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import com.andreykosarygin.game_ui.screen_game.ScreenGame
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ScreenMain(onClickStart = { /*TODO*/ }, onClickBalance = { /*TODO*/ }) {
-
-            }
+            var playDrumAnimation by remember { mutableStateOf(false) }
+            
+            ScreenGame(
+                onClickBack = { /*TODO*/ },
+                onClickInfo = { /*TODO*/ },
+                onClickSpin = {
+                    playDrumAnimation = true
+                },
+                balance = "25",
+                playAnimation = playDrumAnimation,
+                drumIconShowLeft = 4,
+                drumIconShowCenter = 3,
+                drumIconShowRight = 0,
+                drumAnimationFinished = {
+                    playDrumAnimation = false
+                }
+            )
         }
     }
 }
--- game-ui/src/main/java/com/andreykosarygin/game_ui/screen_game/ScreenGame.kt
+++ game-ui/src/main/java/com/andreykosarygin/game_ui/screen_game/ScreenGame.kt
@@ -1,6 +1,9 @@
 package com.andreykosarygin.game_ui.screen_game
 
 import android.annotation.SuppressLint
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -11,12 +14,20 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clipToBounds
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.andreykosarygin.common.LuckyLottoButton
@@ -29,7 +40,8 @@ import com.andreykosarygin.common.R
 @Preview(showBackground = true)
 @Composable
 private fun Preview() {
-    ScreenGame({}, {}, {}, "25")
+    ScreenGame({}, {}, {}, "25", false, 0, 0, 0, {})
+//    SlotMachine(false)
 }
 
 @Composable
@@ -37,7 +49,13 @@ fun ScreenGame(
     onClickBack: () -> Unit,
     onClickInfo: () -> Unit,
     onClickSpin: () -> Unit,
-    balance: String
+    balance: String,
+    playAnimation: Boolean,
+
+    drumIconShowLeft: Int,
+    drumIconShowCenter: Int,
+    drumIconShowRight: Int,
+    drumAnimationFinished: () -> Unit
 ) {
     Box(
         contentAlignment = Alignment.Center,
@@ -58,7 +76,13 @@ fun ScreenGame(
                     modifier = Modifier.padding(top = 14.dp, bottom = 33.dp)
                 )
 
-                SlotMachine()
+                SlotMachine(
+                    playAnimation = playAnimation,
+                    whatIndexNeedToShowLeft = drumIconShowLeft,
+                    whatIndexNeedToShowCenter = drumIconShowCenter,
+                    whatIndexNeedToShowRight = drumIconShowRight,
+                    animationFinishedListener = drumAnimationFinished
+                )
 
                 LuckyLottoButton(
                     text = stringResource(id = R.string.spin),
@@ -67,22 +91,6 @@ fun ScreenGame(
                 )
             }
         }
-    }
-}
-
-@Composable
-fun SlotMachine() {
-    Box(
-        modifier = Modifier.offset(x = 14.dp),
-        contentAlignment = Alignment.Center
-    ) {
-        Image(
-            modifier = Modifier.size(width = 373.dp, height = 244.dp),
-            painter = painterResource(
-                id = com.andreykosarygin.game_ui.R.drawable.slot_machine_empty
-            ),
-            contentDescription = stringResource(id = R.string.content_description_slot_machine)
-        )
     }
 }
 
@@ -157,5 +165,319 @@ fun PointsValue(
             fontSize = 27.16.sp,
             font = Font(resId = R.font.bitter_900)
         )
+    }
+}
+
+@Composable
+fun SlotMachine(
+    playAnimation: Boolean,
+    whatIndexNeedToShowLeft: Int,
+    whatIndexNeedToShowCenter: Int,
+    whatIndexNeedToShowRight: Int,
+    animationFinishedListener: () -> Unit
+) {
+    Box(
+        modifier = Modifier.offset(x = 14.dp),
+        contentAlignment = Alignment.Center
+    ) {
+        Image(
+            modifier = Modifier.size(width = 373.dp, height = 244.dp),
+            painter = painterResource(
+                id = com.andreykosarygin.game_ui.R.drawable.slot_machine_empty
+            ),
+            contentDescription = stringResource(id = R.string.content_description_slot_machine)
+        )
+
+        Box(
+            contentAlignment = Alignment.Center,
+            modifier = Modifier
+                .size(width = 274.dp, height = 105.dp)
+                .offset(x = -(12).dp, y = -(15.5).dp)
+        ) {
+            Drum(
+                playDrumAnimation = playAnimation,
+                whatIndexNeedToShowLeft = whatIndexNeedToShowLeft,
+                whatIndexNeedToShowCenter = whatIndexNeedToShowCenter,
+                whatIndexNeedToShowRight = whatIndexNeedToShowRight,
+                animationFinishedListener = animationFinishedListener
+            )
+        }
+    }
+}
+
+@Composable
+fun Drum(
+    playDrumAnimation: Boolean,
+    whatIndexNeedToShowLeft: Int,
+    whatIndexNeedToShowCenter: Int,
+    whatIndexNeedToShowRight: Int,
+    animationFinishedListener: () -> Unit,
+    easingElementsCount: Int = 5,
+    minimumDurationMillis: Int = 100,
+    minimumShownElementsLeft: Int = 20,
+    minimumShownElementsCenter: Int = 30,
+    minimumShownElementsRight: Int = 48,
+) {
+    val listDrumIcons = remember {
+        mutableStateListOf(
+            com.andreykosarygin.game_ui.R.drawable.slot_machine_icon_bar,
+            com.andreykosarygin.game_ui.R.drawable.slot_machine_icon_jackpot,
+            com.andreykosarygin.game_ui.R.drawable.slot_machine_icon_cherry,
+            com.andreykosarygin.game_ui.R.drawable.slot_machine_icon_apple,
+            com.andreykosarygin.game_ui.R.drawable.slot_machine_icon_bonus,
+            com.andreykosarygin.game_ui.R.drawable.slot_machine_icon_coin,
+            com.andreykosarygin.game_ui.R.drawable.slot_machine_icon_crown,
+            com.andreykosarygin.game_ui.R.drawable.slot_machine_icon_lemon,
+            com.andreykosarygin.game_ui.R.drawable.slot_machine_icon_seven,
+            com.andreykosarygin.game_ui.R.drawable.slot_machine_icon_symbol,
+            com.andreykosarygin.game_ui.R.drawable.slot_machine_icon_watermelon,
+            com.andreykosarygin.game_ui.R.drawable.slot_machine_icon_wild,
+        )
+    }
+
+    var needToStartAnimation by remember { mutableStateOf(true) }
+
+    var playDrumAnimationLeft by remember { mutableStateOf(false) }
+    var playDrumAnimationCenter by remember { mutableStateOf(false) }
+    var playDrumAnimationRight by remember { mutableStateOf(false) }
+
+    if (playDrumAnimation && needToStartAnimation) {
+        playDrumAnimationLeft = true
+        playDrumAnimationCenter = true
+        playDrumAnimationRight = true
+    }
+
+    DrumSectionLeft(
+        animationStartState = playDrumAnimationLeft,
+        whatIndexNeedToShow = whatIndexNeedToShowLeft,
+        listOfDrawableResId = listDrumIcons,
+        finishedListener = {
+            needToStartAnimation = false
+            playDrumAnimationLeft = false
+        },
+        easingElementsCount = easingElementsCount,
+        minimumDurationMillis = minimumDurationMillis,
+        minimumShownElements = minimumShownElementsLeft
+    )
+
+    DrumSectionCenter(
+        animationStartState = playDrumAnimationCenter,
+        whatIndexNeedToShow = whatIndexNeedToShowCenter,
+        listOfDrawableResId = listDrumIcons,
+        finishedListener = {
+            needToStartAnimation = false
+            playDrumAnimationCenter = false
+        },
+        easingElementsCount = easingElementsCount,
+        minimumDurationMillis = minimumDurationMillis,
+        minimumShownElements = minimumShownElementsCenter
+    )
+
+    DrumSectionRight(
+        animationStartState = playDrumAnimationRight,
+        whatIndexNeedToShow = whatIndexNeedToShowRight,
+        listOfDrawableResId = listDrumIcons,
+        finishedListener = {
+            playDrumAnimationRight = false
+            animationFinishedListener()
+            needToStartAnimation = true
+        },
+        easingElementsCount = easingElementsCount,
+        minimumDurationMillis = minimumDurationMillis,
+        minimumShownElements = minimumShownElementsRight
+    )
+}
+
+@Composable
+fun DrumSectionLeft(
+    animationStartState: Boolean,
+    whatIndexNeedToShow: Int,
+    listOfDrawableResId: List<Int>,
+    finishedListener: () -> Unit,
+    easingElementsCount: Int = 5,
+    minimumDurationMillis: Int = 100,
+    minimumShownElements: Int = 30
+) {
+    Box(
+        modifier = Modifier
+            .size(width = 72.dp, height = 105.dp)
+            .offset(x = -(88).dp)
+            .clipToBounds(),
+        contentAlignment = Alignment.Center
+    ) {
+        SlotMachineDrumAnimation(
+            animationStartState = animationStartState,
+            whatIndexNeedToShow = whatIndexNeedToShow,
+            minimumShownElements = minimumShownElements,
+            sizeOfBox = 72.dp,
+            sizeOfInnerBox = 72.dp,
+            listOfDrawableResId = listOfDrawableResId,
+            finishedListener = finishedListener,
+            easingElementsCount = easingElementsCount,
+            minimumDurationMillis = minimumDurationMillis
+        )
+    }
+}
+
+@Composable
+fun DrumSectionCenter(
+    animationStartState: Boolean,
+    whatIndexNeedToShow: Int,
+    listOfDrawableResId: List<Int>,
+    finishedListener: () -> Unit,
+    easingElementsCount: Int = 5,
+    minimumDurationMillis: Int = 100,
+    minimumShownElements: Int = 30
+) {
+    Box(
+        modifier = Modifier
+            .size(width = 72.dp, height = 105.dp)
+            .offset(x = -(1).dp)
+            .clipToBounds(),
+        contentAlignment = Alignment.Center
+    ) {
+        SlotMachineDrumAnimation(
+            animationStartState = animationStartState,
+            whatIndexNeedToShow = whatIndexNeedToShow,
+            minimumShownElements = minimumShownElements,
+            sizeOfBox = 72.dp,
+            sizeOfInnerBox = 72.dp,
+            listOfDrawableResId = listOfDrawableResId,
+            finishedListener = finishedListener,
+            easingElementsCount = easingElementsCount,
+            minimumDurationMillis = minimumDurationMillis
+        )
+    }
+}
+
+@Composable
+fun DrumSectionRight(
+    animationStartState: Boolean,
+    whatIndexNeedToShow: Int,
+    listOfDrawableResId: List<Int>,
+    finishedListener: () -> Unit,
+    easingElementsCount: Int = 5,
+    minimumDurationMillis: Int = 100,
+    minimumShownElements: Int = 30
+) {
+    Box(
+        modifier = Modifier
+            .size(width = 72.dp, height = 105.dp)
+            .offset(x = 85.dp)
+            .clipToBounds(),
+        contentAlignment = Alignment.Center
+    ) {
+        SlotMachineDrumAnimation(
+            animationStartState = animationStartState,
+            whatIndexNeedToShow = whatIndexNeedToShow,
+            minimumShownElements = minimumShownElements,
+            sizeOfBox = 72.dp,
+            sizeOfInnerBox = 72.dp,
+            listOfDrawableResId = listOfDrawableResId,
+            finishedListener = finishedListener,
+            easingElementsCount = easingElementsCount,
+            minimumDurationMillis = minimumDurationMillis
+        )
+    }
+}
+
+@Composable
+fun SlotMachineDrumAnimation(
+    animationStartState: Boolean,
+    whatIndexNeedToShow: Int,
+    minimumShownElements: Int,
+    easingElementsCount: Int = 5,
+    minimumDurationMillis: Int = 100,
+    sizeOfBox: Dp,
+    sizeOfInnerBox: Dp,
+    listOfDrawableResId: List<Int>,
+    finishedListener: () -> Unit
+) {
+    val animationTargetValue = sizeOfInnerBox + ((sizeOfBox - sizeOfInnerBox) / 2)
+
+    var playAnimation by remember { mutableStateOf(animationStartState) }
+    playAnimation = animationStartState
+    var animationIsRunning by remember { mutableStateOf(false) }
+
+    var targetValue by remember { mutableStateOf(0.dp) }
+    var durationMillis by remember { mutableStateOf(minimumDurationMillis) }
+
+    var selectedItemIndex by remember { mutableStateOf(0) }
+    var drawableResId by remember { mutableStateOf(listOfDrawableResId.first()) }
+
+    var counterShownElements by remember { mutableStateOf(0) }
+
+    var targetFastShownElements by remember { mutableStateOf(0) }
+
+
+    val offsetAnimated by animateDpAsState(
+        targetValue = targetValue,
+        animationSpec = tween(
+            durationMillis = durationMillis,
+            easing = LinearEasing
+        ),
+        finishedListener = {
+            if (playAnimation) {
+                if (durationMillis == 0) {
+                    if (counterShownElements > targetFastShownElements) {
+                        if ((counterShownElements - targetFastShownElements) < easingElementsCount) {
+                            durationMillis =
+                                minimumDurationMillis * (counterShownElements - targetFastShownElements)
+                            targetValue = animationTargetValue
+                        } else {
+                            durationMillis =
+                                minimumDurationMillis * (counterShownElements - targetFastShownElements)
+                            targetValue = 0.dp
+                            playAnimation = false
+                            animationIsRunning = false
+                            counterShownElements = 0
+                            finishedListener()
+                        }
+
+                    } else {
+                        durationMillis = minimumDurationMillis
+                        targetValue = animationTargetValue
+                    }
+                } else {
+                    durationMillis = 0
+                    targetValue = -(animationTargetValue)
+
+                    selectedItemIndex =
+                        if (selectedItemIndex != listOfDrawableResId.lastIndex) selectedItemIndex + 1 else 0
+                    drawableResId = listOfDrawableResId[selectedItemIndex]
+                    counterShownElements += 1
+                }
+            }
+        }
+    )
+
+    if (playAnimation) {
+        if (!animationIsRunning) {
+            targetFastShownElements =
+                (((((minimumShownElements / listOfDrawableResId.size) + (if (minimumShownElements % listOfDrawableResId.size > 0) 1 else 0)) * listOfDrawableResId.size) + whatIndexNeedToShow) - easingElementsCount) - selectedItemIndex
+            durationMillis = minimumDurationMillis
+            targetValue = animationTargetValue
+            animationIsRunning = true
+        }
+    }
+
+    Box(
+        modifier = Modifier
+            .size(size = sizeOfBox)
+            .clipToBounds(),
+        contentAlignment = Alignment.Center
+    ) {
+        Box(
+            Modifier
+                .offset(y = offsetAnimated)
+                .size(size = sizeOfInnerBox)
+        ) {
+            Image(
+                modifier = Modifier.fillMaxSize(),
+                painter = painterResource(id = drawableResId),
+                contentScale = ContentScale.Fit,
+                contentDescription = ""
+            )
+        }
     }
 }
